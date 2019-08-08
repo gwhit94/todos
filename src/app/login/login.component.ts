f814cfcff7bdb7edc2ae4090f2a2d43da0392961
Graham <greysnaa@gmail.com>
2019-08-08 16:54:24 -0500

    added login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { UserloginService } from '../userlogin.service';
-import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-login',
@@ -8,18 +7,18 @@ import { stringify } from 'querystring';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  private allUserInfo: Object[] = [];
   username: string = "";
   password: string = "";
-
-
+  
   constructor(private userloginService: UserloginService) {
-
   }
+
   login(){
-    this.userloginService.loginUser(this.username, this.password);
-    this.allUserInfo = this.userloginService.getAllUserInfo();
-    console.log(this.allUserInfo);
+    if (!this.username || !this.password){
+      console.log("enter a username AND password please");
+      return;
+    }
+    this.userloginService.loginUser(this.username);
   }
 
   ngOnInit() {
--- src/app/userlogin.service.ts
+++ src/app/userlogin.service.ts
@@ -1,30 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserloginService {
-  private allUserInfo: Object[] = [];
-  username: string = "";
-  password: string = "";
 
-  'u': string;
-  'p': string;
-  // u = username p = password
-  loginUser(u, p){
-    if (!u || !p){
-      console.log("enter a username AND password please");
-      return;
-    }
-    let userInfo = {
-      username: u,
-      password: p
-    }
-    this.allUserInfo.push(userInfo);
-  }
-  getAllUserInfo(){
-    return this.allUserInfo;
+  constructor(private router: Router) { }
+
+  loginUser(u){
+    localStorage.setItem("user", u);
+    this.router.navigate(['user/'+u]);
   }
 
-  constructor() { }
+  logout(){
+    localStorage.clear();
+  }
+
+  get user(){
+    return localStorage.getItem("user");
+  }
+
+  
 }
