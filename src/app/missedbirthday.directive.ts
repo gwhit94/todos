33e4f2c526a0e68c6cc7810feaaadd32f2f9cc16
Graham <greysnaa@gmail.com>
2019-08-08 15:30:29 -0500

    Finished directives

--- src/app/missedbirthday.directive.ts
+++ src/app/missedbirthday.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, HostBinding, OnInit } from '@angular/core';
 
 @Directive({
   selector: '.birthday'
 })
-export class MissedbirthdayDirective {
+export class MissedbirthdayDirective implements OnInit {
+  @Input() birthday: string;
+  myBday: Date = new Date("06/03/1994");
 
   constructor() { }
+
+  @HostBinding('class.missed') private missed: boolean;
+  @HostBinding('class.is-older') private isOlder: boolean;
+
+  ngOnInit(){
+    let userBday = new Date(this.birthday);
+
+    this.isOlder = userBday < this.myBday;    
+
+    let today = new Date();
+    if(userBday.getMonth() < today.getMonth()){
+      this.missed = true;
+    } else if( (userBday.getMonth() === today.getMonth() ) && (userBday.getDate() < today.getDate()) ){
+      this.missed = true;
+    } else {
+      this.missed = false;
+    }
+  }
+  
 
 }
--- src/app/mousefocus.directive.ts
+++ src/app/mousefocus.directive.ts
@@ -1,9 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: '[mouseFocus]'
+  selector: '[appMouseFocus]'
 })
 export class MousefocusDirective {
+  @HostBinding('style.background-color') private bgColor: string;
+
+  @HostListener("mouseenter") onMouseEnter(){
+    this.bgColor = "blue";
+  }
+  @HostListener("mouseleave") onMouseLeave(){
+    this.bgColor = "purple";
+  }
+  @HostListener("mousedown") onMouseDown(){
+    this.bgColor = "yellow";
+  }
+  @HostListener("mouseup") onMouseUp(){
+    this.bgColor = null;
+  }
 
   constructor() { }
 
